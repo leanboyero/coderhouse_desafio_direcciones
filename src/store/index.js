cd6b9efd52e9e1183129fe191fbf53844ed4fe0e
Leandro Ezequiel Boyero <lboyero@adifse.com.ar>
2022-02-17 23:32:56 -0300

    Desafío: Device features

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import {applyMiddleware, combineReducers, createStore} from 'redux';
+
+import PlacesReducer from './places.reducer';
+import thunk from 'redux-thunk';
+
+// reducers
+
+const RootReducer = combineReducers({
+  places: PlacesReducer,
+});
+
+export default createStore(RootReducer, applyMiddleware(thunk));
